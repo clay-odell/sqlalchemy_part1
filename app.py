91fac7308cf413b0eb398c6454cd1b291d937e1c
Clay O'Dell <clay.odell.music@gmail.com>
2024-05-28 11:32:41 -0500

    user list, and user_homepage, added

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
 """Blogly application."""
-
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template, redirect, request
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:01302@localhost/blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-
+app.config['SECRET_KEY'] = 'mysuperdupersupersecretkey'
 connect_db(app)
-db.create_all()
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def users_list():
+    users = User.query.all()
+    return render_template('/users_list.html', users=users)
+
+@app.route('/add_user_form', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        first_name = request.form['first-name']
+        last_name = request.form['last-name']
+        user_img_url = request.form['img-url']
+
+        user = User(first_name=first_name, last_name=last_name, image_URL=user_img_url)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect('/')
+    else:
+        
+        return render_template('add_user_form.html')
+    
+
+@app.route('/<user_id>')
+def show_user(user_id):
+    """Show Individual Users"""
+    user = User.query.get_or_404(user_id)
+    return render_template('/user_homepage.html', user=user)
+
+
+
+
--- models.py
+++ models.py
@@ -1,1 +1,30 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
 """Models for Blogly."""
+class User (db.Model):
+    """User"""
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key = True,
+                   autoincrement= True)
+    first_name = db.Column(db.String(50),
+                           nullable = False)
+    last_name = db.Column(db.String(50),
+                          nullable =False)
+    image_URL = db.Column(db.String (150),
+                          nullable = True,
+                          default = 'https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg')
+    
+    def __repr__(self):
+        u = self
+        return f"<User {u.id} {u.first_name}{u.lastname}{u.image_url}>"
+
+    
